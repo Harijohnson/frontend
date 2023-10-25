f8a61240b811fad7dd3a13478bc016db570bc209
Harijohnson <harijn72@gmail.com>
2023-10-25 18:17:48 +0530

    Celciues  to fherenheit in js function alterations

--- ctof/script.js
+++ ctof/script.js
@@ -2,8 +2,14 @@ function convert(){
     let cel =Number(document.getElementById("input").value);
     // console.log(cel);
     let fer = cel   *  (9/5)  + 32
-    // console.log(fer);
+    console.log(fer);
     let result = document.getElementById("result");
     // console.log(result);
+
+    if  (fer !== 'NaN'){
     return result.innerHTML = fer.toFixed(2) + "  Fahrenheit ";
+    }
+    else{
+        return result.innerHTML = "Not applicable in here tru some another number.";
+    }
 }
