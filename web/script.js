94cf6b9e0ed6950fcc0d0fc16717f42700f79afd
Harijohnson <harijn72@gmail.com>
2023-10-25 15:55:11 +0530

    'js basics'

--- web/script.js
+++ web/script.js
@@ -1,5 +1,53 @@
 console.log('happy new year')
-alert('new year')
+//alert('new year')
 // comment
+/*  long line comment 
+like this two lines
+in this code */
+
+// varible
+
+let score = 0  // inetilazation
+
+score = 10  //asignment
+
+console.log('score: ' + score)
+
+let amt =20000
+
+amt=10  // this is posible in here
+console.log('total amt in hand is ' + amt)
 
 
+const pi= 3.17
+// pi=232   this is not posible 
+console.log('pi: ' + pi)
+
+//operatorss
+/*
+arithmatic operators   ==>  +   -  *  **  /   %  ++  --
+asignment operators    ==>  +=   -=   *=   **=   /=    %= 
+comparison operators   ==>  == ===    >  <  >=  <=  >==  <== != !==  ?:  
+logical operators      ==>  &&   ||   !
+bitwise operators      ==>   &     ~      !     ^    >>   <<    >>>      <<< 
+special  values  ==> NaN    Undefined    Null   Infinity 
+
+
+typeof function
+
+*/
+
+
+
+//javascript input
+
+
+// //this is not recomandable in websit  please avoid this in web 
+
+// let name  = prompt('Enter your name')
+// console.log('your name' , name)
+
+
+//let num = prompt('Entera a number')
+//console.log(num+12)  // 123+12    op  =  12312
+//console.log(Number(num)+12)  //     123+12   op =    1245
