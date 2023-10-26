aaf56fe84b56dcd3a47f3a63f6a4a831d3f671a8
Harijohnson <harijn72@gmail.com>
2023-10-26 17:30:56 +0530

    switch case

--- switch/script.js
+++ switch/script.js
@@ -1,38 +1,202 @@
-function fdRate(){
+let result = document.createElement('div')
+result.id = 'result';
+result.style.color ='red';
 
-    let months = Number(document.getElementById('moths').value)
-    let amt = Number(document.getElementById('amt').value)
-    console.log(months)
-    console.log(amt)
-    // months
-    // months  < 3   5.8 %
-    //months  3>x<6  6.5 %
-    //months  6>x< 9 6.8 %
-    //months  x>10   7 %
 
-    if (months < 3 ) {
-        let total = ((amt/100) * 5.8)*months
-        var result = document.getElementById('result');
-        result.innerHTML =`Value FD of ${months} months and  ${amt} total income is ${total} `
-        return  result
+function displayStats(){
+    const input = document.querySelector('#input');
+    // console.log('good')
+
+    let city = input.options[input.selectedIndex].value
+    console.log(city)
+    let population=0,lit=0,language=""
+    switch(city) {
+        case 'Ooty':
+            population = 200000
+            lit =98.0
+            language = 'Tamiil'
+            break
+        case 'Chenai':
+            population = 454350000
+            lit =78.8
+            language = 'English'
+            break
+        case 'Madurai':
+            population = 3453534
+            lit =87.0
+            language = 'Tamiil'
+            break
+        case 'Coimbatore':
+            population = 34566465
+            lit =89.0
+            language = 'Kanada'
+            break
+        case 'Ramanatha Puram':
+            population = 87768678
+            lit =76.0
+            language = 'Tamiil'
+            break
     }
-    else  if (months >= 3 && months < 6  ) {
-        let total = ((amt/100) * 6.5)*months
-        var result = document.getElementById('result');
-        result.innerHTML =`Value FD of ${months} months and  ${amt} total income is ${total} `
-        return  result
-    }
-    else  if (months >= 6 && months < 10 ) {
-        let total = ((amt/100) * 6.8)*months
-        var result = document.getElementById('result');
-        result.innerHTML =`Value FD of ${months} months and  ${amt} total income is ${total} `
-        return  result
-    }
-    else  if (months >= 10 ) {
-        let total = ((amt/100) * 7) * months
-        var result = document.getElementById('result');
-        result.innerHTML =`Value FD of ${months} months and  ${amt} total income is ${total} `
-        return  result
-    }
+    let text = `The Indian city of ${city} has a population of ${population}. Language spoken is ${language} and litracye rate of ${lit} %`
+    // console.log(text)
+
+    document.getElementById('wrapper').appendChild(result)
+
+    document.getElementById('result').innerHTML = text
+
 
 }
+
+
+
+
+
+
+const button = document.querySelector('button');
+//Evet listener
+button.addEventListener("click",displayStats);  // for above button added the event listener
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
