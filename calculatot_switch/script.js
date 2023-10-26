d90ff01cb882e0f151160b468fefa86772cda115
Harijohnson <harijn72@gmail.com>
2023-10-26 17:56:36 +0530

    switch case calculator

--- calculatot_switch/script.js
+++ calculatot_switch/script.js
@@ -1,3 +1,26 @@
+result = document.createElement('div');
+result.id ='result';
+document.getElementById('wrapper').appendChild(result);
+
+
+function calculate(){
+    let  calc= document.querySelector("#cal")
+    if (calc.indexOf('+')){
+        calc.split('+')
+        console.log(cal)
+    }
+    if (calc.indexOf('-')){
+        calc.split('-')
+    }
+    if (calc.indexOf('*')){
+        calc.split('*')
+    }
+    if (calc.indexOf('/')){
+        calc.split('/')
+    }
+
+
+}
 
 
 
@@ -7,6 +30,9 @@
 
 
 
+
+let cal = document.querySelector("#cal")
+cal.addEventListener("click", calculate() );
 
 
 
--- switch/script.js
+++ switch/script.js
@@ -1,6 +1,7 @@
 let result = document.createElement('div')
 result.id = 'result';
 result.style.color ='red';
+document.getElementById('wrapper').appendChild(result)
 
 
 function displayStats(){
@@ -40,8 +41,7 @@ function displayStats(){
     let text = `The Indian city of ${city} has a population of ${population}. Language spoken is ${language} and litracye rate of ${lit} %`
     // console.log(text)
 
-    document.getElementById('wrapper').appendChild(result)
-
+   
     document.getElementById('result').innerHTML = text
 
 
