1a658798dcb71e39db07c5f3d68bbe6e2e62fa5d
Harijohnson <harijn72@gmail.com>
2023-10-27 08:08:43 +0530

    objects

--- object/script.js
+++ object/script.js
@@ -1,12 +1,36 @@
-function convert(){
+let item= {
+    name:'acer',
+    price:56000,
+    quantity:1,  
+    buy: function (){
+        console.log('item add to cart with function ');
+    },
 
-let  cm= Number(document.getElementById('input').value);
-// console.log(cm);
-
-// cm=cm/2.54
-
-let inch = cm/2.54;
-
-let op = document.getElementById('result');
-return op.innerHTML = inch.toFixed(2) + '  Inches';
+    addToCart(){
+        console.log('item add to cart without function');
+    }
 }
+console.log(item)
+
+console.log(item.buy())
+console.log(item.addToCart())
+// creaing an object
+let item2= new Object()
+item2.name = 'caherger'
+item2.price = 700
+item2.quantity = 1
+
+console.log(item2)
+
+console.log(item.name)  //acer
+item.price=12  //price =12
+item.size =17  // add new property
+console.log(item)
+
+
+// squre bracket notation
+
+console.log(item['price'])  //price =12
+item['size']=13  //change  the size of the product
+
+
