56e0cf13015aa2c211d10f6f8a090c9ac34925e2
Harijohnson <harijn72@gmail.com>
2023-10-27 11:14:06 +0530

    'new changes'

--- loops/script.js
+++ loops/script.js
@@ -1,3 +1,61 @@
-for (let i =1 ;i<=5;i++){
-    console.log(i)
-}
+// for (let  i  =1 ;i<=5;i++){
+//     console.log(i)
+// }
+
+
+
+// for (let j=10 ;j>=1;j--){
+//     console.log(j)
+// }
+
+// i=10;
+
+// while (i>=0){
+//     console.log(i)
+//     i--;
+//     // break
+// }
+
+// while (true){
+//     let num= Number(prompt('Enter a number'))
+//     if (!isNaN(num)){
+//         console.log(num)
+//         break
+//     }
+// }
+
+let arr =['az','b','c','d','e','f','g','h']
+
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+
+
+
+
+
+
+// for (let a of arr){
+//     for (let b of a){
+//         console.log(b)
+//     }
+//     // console.log(a)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
